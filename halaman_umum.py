8a7854cb3bbbf7f9f92426ad8778936253dde365
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2025-06-07 10:35:20 +0700

    Update halaman_umum.py

--- halaman_umum.py
+++ halaman_umum.py
@@ -1,30 +1,32 @@
+from streamlit_option_menu import option_menu
 import streamlit as st
-import pandas as pd
-from datetime import datetime
-import pytz
 
-# Mendapatkan waktu Jakarta
-jakarta_tz = pytz.timezone('Asia/Jakarta')
-now = datetime.now(jakarta_tz)
+# Page config
+st.set_page_config(page_title="Food Time Delivery Project", layout="wide")
 
-# Menentukan waktu dalam kategori
-if 5 <= now.hour < 11:
-    greeting = "Selamat Pagi"
-elif 11 <= now.hour < 15:
-    greeting = "Selamat Siang"
-elif 15 <= now.hour < 18:
-    greeting = "Selamat Sore"
-else:
-    greeting = "Selamat Malam"
+# NAVIGATION BAR (ATAS)
+st.title("Food Time Delivery Tools")
+st.info("Selamat Datang di Website Case Food Time Delivery Tools!")
+selected = option_menu(
+    menu_title= None,
+    options=["Introduction", "EDA", "Prediction Tools"],
+    icons=["book", "archive","tools"],
+    orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#f0f2f6"},
+        "icon": {"color": "black", "font-size": "18px"},
+        "nav-link": {
+            "font-size": "16px",
+            "text-align": "center",
+            "margin": "0px",
+            "--hover-color": "#eee",
+        },
+        "nav-link-selected": {"background-color": "#ff4b4b", "color": "white"},
+    }
+)
 
-def tampilkan_halaman_umum():
-    st.info("Selamat datang di Prediction Tools!")
-    st.markdown(f"""
-    - **{greeting}**, Diharapkan digunakan dengan baik!
-                **Food Time Delivery Prediction**.    
-    - **Menu Tab terdapat Exploratory Data Analysis (EDA) dan Prediction Test**.
-    - Pada Menu EDA sebagai visualisasi atau analisis statistik terkait kasus Food Time Delivery.
-    - Pada Menu Prediction Test terdapat Prediction Test yang digunakan untuk menguji hasil prediksi waktu pengantaran (Food Time Delivery) dari variabel-variabel yang tersedia
-    - Tools ini dibuat oleh: Ferdyansyah Permana Putra - DS30.
-    """)
+# SECTION CONTROL
+if selected == "Introduction":
+    import intro
+    intro.tampilkan_halaman_umum()
 
