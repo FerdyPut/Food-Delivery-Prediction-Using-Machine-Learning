aae009689005568fe38ff25e305cc39e26986277
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2025-06-07 10:39:10 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -18,13 +18,27 @@ else:
     greeting = "Selamat Malam"
 
 def tampilkan_halaman_umum():
-    st.info("Selamat datang di Prediction Tools!")
     st.markdown(f"""
-    - **{greeting}**, Diharapkan digunakan dengan baik!
-                **Food Time Delivery Prediction**.    
-    - **Menu Tab terdapat Exploratory Data Analysis (EDA) dan Prediction Test**.
-    - Pada Menu EDA sebagai visualisasi atau analisis statistik terkait kasus Food Time Delivery.
-    - Pada Menu Prediction Test terdapat Prediction Test yang digunakan untuk menguji hasil prediksi waktu pengantaran (Food Time Delivery) dari variabel-variabel yang tersedia
-    - Tools ini dibuat oleh: Ferdyansyah Permana Putra - DS30.
-    """)
+    <style>
+        .hover-box {{
+            background-color: #FFEB3B; /* Kuning */
+            padding: 10px;
+            border-radius: 5px;
+            transition: transform 0.3s ease, box-shadow 0.3s ease;
+        }}
+        .hover-box:hover {{
+            transform: scale(1.05); /* Zoom saat hover */
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1); /* Efek bayangan */
+        }}
+    </style>
 
+    <div class="hover-box">
+        <ul>
+            <li><b>{greeting}</b>, Diharapkan digunakan dengan baik! <b>Food Time Delivery Prediction</b>.</li>
+            <li><b>Menu Tab</b> terdapat Exploratory Data Analysis (EDA) dan Prediction Test.</li>
+            <li>Pada Menu <b>EDA</b> sebagai visualisasi atau analisis statistik terkait kasus Food Time Delivery.</li>
+            <li>Pada Menu <b>Prediction Test</b> terdapat Prediction Test yang digunakan untuk menguji hasil prediksi waktu pengantaran (Food Time Delivery) dari variabel-variabel yang tersedia.</li>
+            <li><b>Tools ini dibuat oleh:</b> Ferdyansyah Permana Putra - DS30.</li>
+        </ul>
+    </div>
+    """, unsafe_allow_html=True)
