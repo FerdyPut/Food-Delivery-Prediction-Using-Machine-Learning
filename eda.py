a8f844f71e1ac82300f7d27283410e05cfe3f792
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2025-06-07 11:59:38 +0700

    Update eda.py

--- eda.py
+++ eda.py
@@ -28,49 +28,48 @@ def eda():
         st.write(df.head())  # Menampilkan 5 baris pertama untuk verifikasi
 
         st.markdown("""
-            <style>
-            .insight-box {
-                padding: 15px;
-                background-color: #f9f9f9;
-                border-left: 6px solid #007acc;
-                margin-bottom: 15px;
-            }
-            .insight-box ul {
-                list-style-type: none;
-                padding-left: 20px;
-            }
-            .insight-box h4 {
-                color: #007acc;
-                font-size: 16px;
-                font-weight: bold;
-            }
-            .insight-box li {
-                font-size: 14px;
-                font-weight: normal;
-                color: #555;
-            }
-            </style>
-            
-            <div class="insight-box">
-                <h4>Data Food Time Delivery tersebut merupakan data yang didapatkan dari Kaggle dengan rincian variabel-variabelnya:</h4>
-                
-                <strong>Kategorik:</strong>
-                <ul>
-                    <li><strong>Weather</strong>: Kondisi cuaca pada saat pengiriman (misalnya: Clear, Rainy, etc.)</li>
-                    <li><strong>Traffic_Level</strong>: Tingkat kemacetan lalu lintas (misalnya: Low, Moderate, High)</li>
-                    <li><strong>Time_of_Day</strong>: Waktu pengiriman dilakukan (misalnya: Morning, Afternoon, Evening)</li>
-                    <li><strong>Vehicle_Type</strong>: Jenis kendaraan yang digunakan oleh kurir untuk pengiriman (misalnya: Motor, Car, etc.)</li>
-                </ul>
-                
-                <strong>Numerik:</strong>
-                <ul>
-                    <li><strong>Distance_km</strong>: Jarak yang ditempuh oleh kurir dalam kilometer untuk pengiriman</li>
-                    <li><strong>Courier_Experience_yrs</strong>: Pengalaman kurir dalam tahun</li>
-                    <li><strong>Delivery_Time_min</strong>: Waktu yang dibutuhkan untuk pengiriman dalam menit</li>
-                </ul>
-            </div>
-            """, unsafe_allow_html=True)
+        <style>
+        .insight-box {
+            padding: 15px;
+            background-color: #f9f9f9;
+            border-left: 6px solid #007acc;
+            margin-bottom: 15px;
+        }
+        .insight-box ul {
+            list-style-type: none;
+            padding-left: 20px;
+        }
+        .insight-box h4 {
+            color: #007acc;
+            font-size: 16px;
+            font-weight: bold;
+        }
+        .insight-box li {
+            font-size: 14px;
+            font-weight: normal;
+            color: #555;
+        }
+        </style>
 
+        <div class="insight-box">
+            <h4>Data Food Time Delivery tersebut merupakan data yang didapatkan dari Kaggle dengan rincian variabel-variabelnya:</h4>
+            
+            <strong>Kategorik:</strong>
+            <ul>
+                <li><strong>Weather</strong>: Kondisi cuaca pada saat pengiriman (misalnya: Clear, Rainy, etc.)</li>
+                <li><strong>Traffic_Level</strong>: Tingkat kemacetan lalu lintas (misalnya: Low, Moderate, High)</li>
+                <li><strong>Time_of_Day</strong>: Waktu pengiriman dilakukan (misalnya: Morning, Afternoon, Evening)</li>
+                <li><strong>Vehicle_Type</strong>: Jenis kendaraan yang digunakan oleh kurir untuk pengiriman (misalnya: Motor, Car, etc.)</li>
+            </ul>
+            
+            <strong>Numerik:</strong>
+            <ul>
+                <li><strong>Distance_km</strong>: Jarak yang ditempuh oleh kurir dalam kilometer untuk pengiriman</li>
+                <li><strong>Courier_Experience_yrs</strong>: Pengalaman kurir dalam tahun</li>
+                <li><strong>Delivery_Time_min</strong>: Waktu yang dibutuhkan untuk pengiriman dalam menit</li>
+            </ul>
+        </div>
+    """, unsafe_allow_html=True)
     # Statistik Deskriptif
     with st.expander("Descriptive Statistics"):
         st.write(df.describe())
