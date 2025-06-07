6dd6b7ea806bd789055a4b3904c429f2994da9c7
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2025-06-07 13:24:56 +0700

    Update prediksi.py

--- prediksi.py
+++ prediksi.py
@@ -29,8 +29,8 @@ def prediksi():
     vehicle_type = st.selectbox("Jenis Kendaraan", ['Bike', 'Car', 'Scooter'])  # Kategorik
 
     # Membuat DataFrame untuk input pengguna
-    input_data = pd.DataFrame([[distance_km, preparation_time_min, courier_experience_yrs, weather, traffic_level,vehicle_type, time_of_day ]],
-                              columns=['Distance_km', 'Preparation_Time_min', 'Courier_Experience_yrs', 'Weather', 'Traffic_Level','Vehicle_Type', 'Time_of_Day', ])
+    input_data = pd.DataFrame([[distance_km, preparation_time_min, courier_experience_yrs, weather, traffic_level, vehicle_type, time_of_day]],
+                              columns=['Distance_km', 'Preparation_Time_min', 'Courier_Experience_yrs', 'Weather', 'Traffic_Level', 'Vehicle_Type', 'Time_of_Day'])
 
     # Memuat model Linear Regression yang telah disimpan
     model_url = "https://dl.dropboxusercontent.com/scl/fi/hfj45bbigcyvsup9aeipf/Model-Linear-Regression.pkl?rlkey=vggmunmapqzgthbcj2puiohu6"
@@ -44,15 +44,12 @@ def prediksi():
     weather_ohe_url = "https://dl.dropboxusercontent.com/scl/fi/fnjz9ovnvs66cut7e8jig/Weather_ohe.pkl?rlkey=2my2uri7b7fgcmrdtrt2eoak4"
     weather_ohe = load_pickle_from_dropbox(weather_ohe_url)
 
-    # Memuat OneHotEncoder untuk Vehicle_Type dari Dropbox
     vehicle_type_ohe_url = "https://dl.dropboxusercontent.com/scl/fi/nnsljx70zjg3evnd51zxy/Vehicle_Type_ohe.pkl?rlkey=sc6giyo06e7yia0flfrtuxj5m"
     vehicle_type_ohe = load_pickle_from_dropbox(vehicle_type_ohe_url)
 
-    # Memuat OneHotEncoder untuk Time_of_Day dari Dropbox
     time_of_day_ohe_url = "https://dl.dropboxusercontent.com/scl/fi/e3d20osm0tjnnbtp7ry71/Time_of_Day_ohe.pkl?rlkey=u22cw4pa4h1xsd08dwxit8z7s"
     time_of_day_ohe = load_pickle_from_dropbox(time_of_day_ohe_url)
 
-    # Memuat OneHotEncoder untuk Traffic_Level dari Dropbox
     traffic_level_ohe_url = "https://dl.dropboxusercontent.com/scl/fi/7mj2z6piodp0x9p6y1sq2/Traffic_Level_ohe.pkl?rlkey=pbs9e5d7xvlow74tyt01d7rng"
     traffic_level_ohe = load_pickle_from_dropbox(traffic_level_ohe_url)
 
@@ -71,47 +68,27 @@ def prediksi():
 
             # Menambahkan hasil encoding ke DataFrame input
             input_data = pd.concat([input_data, df_encoded_weather], axis=1)
-
-            # Menghapus kolom asli yang kategorikal
             input_data = input_data.drop(columns=['Weather'])
-            
+
             # Melakukan One-Hot Encoding untuk kolom 'Traffic_Level' menggunakan encoder yang sudah dilatih
             encoded_data_traffic_level = traffic_level_ohe.transform(input_data[['Traffic_Level']])
             encoded_columns_traffic_level = traffic_level_ohe.get_feature_names_out(['Traffic_Level'])
-
-            # Membuat DataFrame hasil encoding untuk 'Traffic_Level'
             df_encoded_traffic_level = pd.DataFrame(encoded_data_traffic_level, columns=encoded_columns_traffic_level, index=input_data.index)
-            
-            # Menambahkan hasil encoding ke DataFrame input
             input_data = pd.concat([input_data, df_encoded_traffic_level], axis=1)
-
-            # Menghapus kolom asli yang kategorikal
             input_data = input_data.drop(columns=['Traffic_Level'])
-           
+
             # Melakukan One-Hot Encoding untuk kolom 'Vehicle_Type' menggunakan encoder yang sudah dilatih
             encoded_data_vehicle_type = vehicle_type_ohe.transform(input_data[['Vehicle_Type']])
             encoded_columns_vehicle_type = vehicle_type_ohe.get_feature_names_out(['Vehicle_Type'])
-
-            # Membuat DataFrame hasil encoding untuk 'Vehicle_Type'
             df_encoded_vehicle_type = pd.DataFrame(encoded_data_vehicle_type, columns=encoded_columns_vehicle_type, index=input_data.index)
-
-            # Menambahkan hasil encoding ke DataFrame input
             input_data = pd.concat([input_data, df_encoded_vehicle_type], axis=1)
-
-            # Menghapus kolom asli yang kategorikal
             input_data = input_data.drop(columns=['Vehicle_Type'])
 
             # Melakukan One-Hot Encoding untuk kolom 'Time_of_Day' menggunakan encoder yang sudah dilatih
             encoded_data_time_of_day = time_of_day_ohe.transform(input_data[['Time_of_Day']])
             encoded_columns_time_of_day = time_of_day_ohe.get_feature_names_out(['Time_of_Day'])
-
-            # Membuat DataFrame hasil encoding untuk 'Time_of_Day'
             df_encoded_time_of_day = pd.DataFrame(encoded_data_time_of_day, columns=encoded_columns_time_of_day, index=input_data.index)
-
-            # Menambahkan hasil encoding ke DataFrame input
             input_data = pd.concat([input_data, df_encoded_time_of_day], axis=1)
-
-            # Menghapus kolom asli yang kategorikal
             input_data = input_data.drop(columns=['Time_of_Day'])
 
             # Menampilkan DataFrame yang telah di-encode
@@ -119,23 +96,22 @@ def prediksi():
             st.write(input_data)
 
             # Melakukan scaling untuk kolom numerik
-            input_data_numeric = input_data[['Distance_km','Preparation_Time_min', 'Courier_Experience_yrs' ]] 
-
-            # Melakukan scaling menggunakan StandardScaler yang telah dilatih
+            input_data_numeric = input_data[['Distance_km','Preparation_Time_min', 'Courier_Experience_yrs']] 
             input_data_scaled = scaler.transform(input_data_numeric)
 
             # Menyusun kembali DataFrame setelah scaling
             input_data_scaled_df = pd.DataFrame(input_data_scaled, columns=input_data_numeric.columns, index=input_data.index)
 
             # Menambahkan hasil scaling ke input_data yang sudah diencoding
-            input_data = pd.concat([input_data_scaled_df, input_data.drop(columns=['Distance_km','Preparation_Time_min', 'Courier_Experience_yrs', ])], axis=1)
+            input_data = pd.concat([input_data_scaled_df, input_data.drop(columns=['Distance_km','Preparation_Time_min', 'Courier_Experience_yrs'])], axis=1)
 
             # Menyusun ulang kolom agar sesuai dengan urutan yang diinginkan
-            correct_column_order = [ 
+            correct_column_order = [
                 'Weather_Clear', 'Weather_Foggy', 'Weather_Rainy', 'Weather_Snowy', 'Weather_Windy',
                 'Traffic_Level_Low', 'Traffic_Level_Medium', 'Traffic_Level_High', 
                 'Vehicle_Type_Bike', 'Vehicle_Type_Car', 'Vehicle_Type_Scooter', 
-                'Time_of_Day_Afternoon', 'Time_of_Day_Evening', 'Time_of_Day_Morning', 'Time_of_Day_Night', 'Preparation_Time_min', 'Courier_Experience_yrs','Distance_km'
+                'Time_of_Day_Afternoon', 'Time_of_Day_Evening', 'Time_of_Day_Morning', 'Time_of_Day_Night',
+                'Preparation_Time_min', 'Courier_Experience_yrs', 'Distance_km'
             ]
             input_data = input_data[correct_column_order]  # Menyesuaikan urutan kolom
 
